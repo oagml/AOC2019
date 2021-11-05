0a11ae25cc3e4f53d789d63158e84070c3992533
oagml <41304974+oagml@users.noreply.github.com>
2021-11-04 18:36:14 -0600

    Add files via upload

---
+++ 10/day10.py
@@ -0,0 +1,21 @@
+"""
+This is an orchard tree problem. Some ideas to solve this puzzle:
+    Use a matrix to store the positions of asteroids and empty spaces
+    Linear algebra could be useful
+    Move the origin to each asteroid to analyze that position
+"""
+#Import the data and convert into a matrix
+
+data = open('inputdata.txt', 'r').read().splitlines()
+
+print(data)
+
+#xlength = len(data[0]) #Get the length of one line (All lines have the same length)
+#ylength = len(data) #Get the length of the list
+
+#Once you have the matrix loop through every position in the matrix and analyse how many asteroids
+#they can see
+
+
+#Determine the asteroid with the highest number of visible asteroids
+
