ca2b0759ba9ef17ffc9a495c2e57440126e77dae
Os <oscar.agml@gmail.com>
2021-11-07 14:44:37 -0600

    Solved Part 2, sloppily

--- 10/p2day10.py
+++ 10/p2day10.py
@@ -7,6 +7,7 @@ destroyed.
 '''
 
 import numpy as np
+import collections
 
 data = open('inputdata.txt', 'r').read().splitlines()
 
@@ -24,8 +25,8 @@ for row  in data:
 
 print(matrix)
 
-stationx = 29 #Monitoring station at x coordinates
-stationy = 26 #Monitoring station at y coordinates
+stationx = 29 #Monitoring station row coordinates
+stationy = 26 #Monitoring station column coordinates
 
 '''
 This function determines the slope and quadrant of a given point in a matrix relative to
@@ -33,40 +34,67 @@ the origin, it returns a tuple with the calculated slope and quadrant
 '''
 def determineSlopeQuadrant(xOrigin, yOrigin, xTarget, yTarget):
 
-    if ((yOrigin - yTarget) == 0): #Avoid division by zero
-        slope = -999
+    if ((yOrigin - yTarget) == 0 and xOrigin<=xTarget): #is above origin
+        slope = 9999
+
+    elif ((yOrigin - yTarget) == 0 and xOrigin>xTarget): 
+        slope = 1000
 
     else:
-        slope = (xOrigin-xTarget)/(yOrigin-yTarget)
+        slope = abs(round((xOrigin-xTarget)/(yOrigin-yTarget), 3))
 
-    if (xTarget>=xOrigin and yTarget>yOrigin):
-        quadrant = "I"
+        if (xTarget<xOrigin and yTarget>=yOrigin):
+            #quadrant = "I"
+            slope += 3000
 
-    elif (xTarget<xOrigin and yTarget>=yOrigin):
-        quadrant = "II"
+        elif (xTarget<=xOrigin and yTarget<yOrigin):
+            #quadrant = "II"
+            slope = 500 - slope
 
-    elif (xTarget<=xOrigin and yTarget<yOrigin):
-        quadrant = "III"
+        elif (xTarget>xOrigin and yTarget<=yOrigin):
+            #quadrant = "III"
+            slope += 1000
 
-    elif (xTarget>xOrigin and yTarget<=yOrigin):
-        quadrant = "IV"
+        elif (xTarget>=xOrigin and yTarget>yOrigin):
+            #quadrant = "IV"
+            slope = 2000 - slope
 
-    else:
-        quadrant = "NA"
+        else:
+            quadrant = "NA"
 
-    return(slope,quadrant)
+    return(slope)
+        
 
 #From the monitoring station, examine all other asteroids that are visible and store them in
 #"groups" of the same slope, each entry is going to contain the x and y position and the distance
 #to the monitoring station  (manhattan distance)
-groups = dict()
+groups = collections.defaultdict(list)
 
 for i in range(xlength):        #columns
     for j in range(ylength):    #rows
-        currentSlope = determineSlopeQuadrant(stationx, stationy, i, j)
-        
+        if (matrix[i][j] == b'#'): #If an asteroid is found
+            if (i == stationx and j == stationy):
+                pass
+            else:
+                currentSlope = determineSlopeQuadrant(stationx, stationy, i, j)
+                currentDist = abs(stationx - i) + abs(stationy - j) #Manhattan distance
+                groups[currentSlope].append([currentDist, i, j])
 
 #Once you have the groups,sort them from the smallest to the greatest distance 
+for i in groups:
+    groups[i].sort()
+
+#for i in groups:
+#    print(i, groups[i])
+
+groupcp = sorted(groups.items(), key = None, reverse = True)
+
+count = 0
+for i in groupcp:
+    count += 1
+    print(count)
+    print(i,)
+
 
 #Sort the groups in a clockwise order, starting from straight up.
 
